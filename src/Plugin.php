a236fe509b5417c419bbaa69af7978262845ceb9
Sergey Romanenko <sergey.romanenko@flextype.org>
2022-11-20 18:36:04 +0800

    chore(core): initial code commit

---
+++ src/Plugin.php
@@ -0,0 +1,15 @@
+<?php
+
+declare(strict_types=1);
+
+namespace ReadingTime;
+
+class Plugin extends \craft\base\Plugin
+{
+    public function init()
+    {
+        parent::init();
+
+        // Custom initialization code goes here...
+    }
+}
